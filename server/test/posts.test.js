b7c66794702baa1ff674a59f5d8aff2a8c64dbf5
carloskauan <carloskauan190@gmail.com>
2022-04-25 14:27:27 -0300

    add tests get, update, delete, insert

--- server/test/posts.test.js
+++ server/test/posts.test.js
@@ -11,14 +11,33 @@ const generateNumber = function () {
 }
 
 const request = function (url, method, data) {
-    return axios({ url, method, data });
+    return axios({
+        url,
+        method,
+        data
+    });
 };
 
-test('Should get posts', async function(){
+test('Should get a posts', async function () {
     // given -> dado que 
-    const post1 = await postService.savePost({ nome: generate(), dono: generate(), raca: generate(), idade: generateNumber()});
-    const post2 = await postService.savePost({ nome: generate(), dono: generate(), raca: generate(), idade: generateNumber()});
-    const post3 = await postService.savePost({ nome: generate(), dono: generate(), raca: generate(), idade: generateNumber()});
+    const post1 = await postService.savePost({
+        nome: generate(),
+        dono: generate(),
+        raca: generate(),
+        idade: generateNumber()
+    });
+    const post2 = await postService.savePost({
+        nome: generate(),
+        dono: generate(),
+        raca: generate(),
+        idade: generateNumber()
+    });
+    const post3 = await postService.savePost({
+        nome: generate(),
+        dono: generate(),
+        raca: generate(),
+        idade: generateNumber()
+    });
     // when -> quando acontecer
     const response = await request('http://localhost:3333/pets', 'get');
     const post = response.data;
@@ -29,9 +48,14 @@ test('Should get posts', async function(){
     await postService.deletePost(post3.id)
 });
 
-test('Should save posts', async function(){
+test('Should save a posts', async function () {
     // given -> dado que 
-    const data = { nome: generate(), dono: generate(), raca: generate(), idade: generateNumber()};
+    const data = {
+        nome: generate(),
+        dono: generate(),
+        raca: generate(),
+        idade: generateNumber()
+    };
     // when -> quando acontecer
     const response = await request('http://localhost:3333/pets', 'post', data);
     const post = response.data;
@@ -42,4 +66,43 @@ test('Should save posts', async function(){
     expect(post.idade).toBe(data.idade)
     await postService.deletePost(post.id)
 
+});
+
+test('Should update a posts', async function () {
+    // given -> dado que 
+    const post = await postService.savePost({
+        nome: generate(),
+        dono: generate(),
+        raca: generate(),
+        idade: generateNumber()
+    });
+    post.nome = generate();
+    post.dono = generate();
+    post.raca = generate();
+    post.idade = generateNumber();
+    // when -> quando acontecers
+    await request(`http://localhost:3333/pets/${post.id}`, 'put', post);
+
+    // then -> então
+    const updatePost = await postService.getPost(post.id);
+    expect(updatePost.nome).toBe(post.nome)
+    expect(updatePost.dono).toBe(post.dono)
+    expect(updatePost.raca).toBe(post.raca)
+    expect(updatePost.idade).toBe(post.idade)
+    await postService.deletePost(post.id);
+
+});
+
+test('Should delete a posts', async function () {
+    // given -> dado que 
+    const post = await postService.savePost({
+        nome: generate(),
+        dono: generate(),
+        raca: generate(),
+        idade: generateNumber()
+    });
+    // when -> quando acontecer
+    await request(`http://localhost:3333/pets/${post.id}`, 'delete');
+    const posts = await postService.getPorts();
+    expect(posts).toHaveLength(0);
 });

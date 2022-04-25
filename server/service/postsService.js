6d6fd9c2e9902ca1994f9ea308ae587d8952e166
carloskauan <carloskauan190@gmail.com>
2022-04-25 14:26:57 -0300

    add services get, update, delete, insert

--- server/service/postsService.js
+++ server/service/postsService.js
@@ -4,11 +4,19 @@ exports.getPorts = function () {
     return postsData.getPorts();
 };
 
+exports.getPost = function (id) {
+	return postsData.getPost(id);
+};
+
 exports.savePost = function (post) {
     return postsData.savePost(post);
 };
 
 exports.deletePost = function (id) {
     return postsData.deletePost(id);
-}
+};
+
+exports.updatePost = function (id, post) {
+    return postsData.updatePost(id, post);
+};
 

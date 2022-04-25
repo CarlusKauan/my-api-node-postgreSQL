8280f1cfcf0726a4f00741805672fcdf6f0bf062
carloskauan <carloskauan190@gmail.com>
2022-04-25 14:23:12 -0300

    add get, update, delete, insert

--- server/data/PostsData.js
+++ server/data/PostsData.js
@@ -4,8 +4,16 @@ exports.getPorts = function () {
     return database.query('select * from pet.post');
 };
 
+exports.getPost = function (id) {
+	return database.oneOrNone('select * from pet.post where id = $1', [id]);
+};
+
 exports.savePost = function (post) {
     return database.one('insert into pet.post (nome, dono, raca, idade) values ($1, $2, $3, $4) returning *', [post.nome, post.dono, post.raca, post.idade]);
+};
+
+exports.updatePost = function (id, post) {
+    return database.none('update pet.post set nome = $1, dono = $2, raca = $3, idade = $4 where id = $5', [post.nome, post.dono, post.raca, post.idade, id]);
 };
 
 exports.deletePost = function (id) {
